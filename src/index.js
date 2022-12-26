5a10272a46b14f4c9306320de5a9ef77d988963d
etmjr03 <etmjr03@gmail.com>
2022-12-26 15:26:12 -0300

    Adicionando funcao helper e renderizador 3D

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import updateCompiler from "webpack-dev-server/lib/utils/updateCompiler";
 
 let sizes = {
     width: window.innerWidth,
@@ -17,21 +16,55 @@ const ASPECT_CAMERA = sizes.width / sizes.height;
 const PERSPECTIVE_CAMERA = new THREE.PerspectiveCamera(120, ASPECT_CAMERA, 0.5, 100);
 const CONTROLS = new OrbitControls(PERSPECTIVE_CAMERA, CANVAS);
 const GRID_HELPER = new THREE.GridHelper(20, 10);
+const GLTF_LOADER = new GLTFLoader();
 const RENDERER = new THREE.WebGLRenderer({
     canvas: CANVAS
 });
 
-DIRECTIONAL_LIGHT.position.set(0, 1, 0)
-PERSPECTIVE_CAMERA.position.set(0, 0, 2)
+DIRECTIONAL_LIGHT.position.set(0, 1, 0);
+PERSPECTIVE_CAMERA.position.set(0, 0, 2);
 
-SCENE.add(DIRECTIONAL_LIGHT, AMBIENT_LIGHT, PERSPECTIVE_CAMERA, GRID_HELPER)
+SCENE.add(DIRECTIONAL_LIGHT, AMBIENT_LIGHT, PERSPECTIVE_CAMERA, GRID_HELPER);
 
-RENDERER.setSize(sizes.width, sizes.height)
-RENDERER.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderSizePixel();
+
+function importModels() {
+    GLTF_LOADER.load('models/gladiator.glb',
+    (gltf) => {
+        SCENE.add(gltf.scene)
+    },
+
+    (progress) => {
+        console.log(progress.loaded, progress.total)
+    },
+
+    (error) => {
+        console.error(error)
+    });
+}
 
 function updateCanvas() {
     RENDERER.render(SCENE, PERSPECTIVE_CAMERA)
     window.requestAnimationFrame(updateCanvas)
 }
 
+function renderSizePixel() {
+    RENDERER.setSize(sizes.width, sizes.height)
+    RENDERER.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    PERSPECTIVE_CAMERA.aspect = ASPECT_CAMERA
+    PERSPECTIVE_CAMERA.updateProjectionMatrix()
+
+    RENDERER.setSize(sizes.width, sizes.height)
+    RENDERER.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    renderSizePixel();
+})
+
+importModels();
 updateCanvas();
